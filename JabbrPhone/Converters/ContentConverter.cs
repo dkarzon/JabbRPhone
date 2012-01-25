295b18f955684faef31fa620ae29dda298809eba
dkarzon <dkarzon@tboda.com>
2012-01-25 19:37:10 +1000

    Put in a ContentConverter to fix up the encoding on messages (maybe can use it for links and images later?) Also put in an xap for people wanting to test it out.

---
+++ JabbrPhone/Converters/ContentConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Data;
+
+namespace JabbrPhone.Converters
+{
+    public class ContentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null) return null;
+
+            return HttpUtility.HtmlDecode(value.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
