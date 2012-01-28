20faaf3be29471e04a575eef56cb3a769c0c8f28
dkarzon <dkarzon@tboda.com>
2012-01-28 10:58:00 +1000

    Updated SignalR, refactored all of the signalR stuff a bit to make it easier to restart the connection (after changing server urls)

---
+++ JabbrPhone/App.xaml.jabbr.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using SignalR.Client.Hubs;
+using JabbrPhone.Models;
+using System.Threading.Tasks;
+using SignalR.Client.Transports;
+using JabbrPhone.Helpers;
+
+namespace JabbrPhone
+{
+    public partial class App
+    {
+        public static HubConnection JabbrConnection { get; set; }
+        public static IHubProxy ChatHub { get; set; }
+
+        /// <summary>
+        /// A helper class that managers server events across the app
+        /// </summary>
+        public SignalRHelper EventManager { get; set; }
+
+        /// <summary>
+        /// This method should be run before the signalR connection is started
+        /// to subscribe to the events
+        /// </summary>
+        internal static void SetupEvents()
+        {
+            App.ChatHub.On<MessageModel, string>("AddMessage",
+                (message, roomName) =>
+                {
+                    ((App)App.Current).EventManager.OnNewMessage(message, roomName);
+                });
+
+            //Other events we need to watch?
+
+            //TODO - User messages
+        }
+
+        /// <summary>
+        /// This method initialises the signalR connection to the server and Starts it
+        /// </summary>
+        /// <param name="startContinue"></param>
+        internal static void InitializeJabbr(Action<Task> startContinue)
+        {
+            App.JabbrConnection = new HubConnection(SettingsHelper.GetServerUrl());
+            App.ChatHub = App.JabbrConnection.CreateProxy("JabbR.Chat");
+
+            var id = Storage.SettingsStorage.GetSetting("id");
+
+            if (!string.IsNullOrEmpty(id))
+            {
+                App.ChatHub["id"] = id;
+            }
+
+            //Setup the signalR events!
+            SetupEvents();
+
+            //Server sent events dont work at the moment...
+            App.JabbrConnection.Start(Transport.LongPolling)
+                .ContinueWith(startContinue);
+
+        }
+
+    }
+}
---
+++ JabbrPhone/Helpers/SettingsHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace JabbrPhone.Helpers
+{
+    public static class SettingsHelper
+    {
+        public static string GetServerUrl()
+        {
+            var serverurl = Storage.SettingsStorage.GetSetting("serverurl");
+            if (string.IsNullOrEmpty(serverurl))
+            {
+                //default
+                return "http://jabbr.net/";
+            }
+            return serverurl;
+        }
+
+        public static void SaveServerUrl(string url)
+        {
+            Storage.SettingsStorage.SaveSetting("serverurl", url);
+        }
+    }
+}
