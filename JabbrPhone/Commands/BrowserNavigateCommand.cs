6ac6ff5c35cfb415aad6aaf8080c3a4cab0b0abf
dkarzon <dkarzon@tboda.com>
2012-01-31 22:18:41 +1000

    Added the Navigate command so that hyperlinks in messages open up a browser task and navigate to the url.

---
+++ JabbrPhone/Commands/BrowserNavigateCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Tasks;
+
+namespace JabbrPhone.Commands
+{
+    public class BrowserNavigateCommand : ICommand
+    {
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            var uri = (Uri)parameter;
+
+            var task = new WebBrowserTask();
+            task.Uri = uri;
+            task.Show();
+        }
+    }
+}
